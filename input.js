e25da34abe53a26a428a9b23432cbb6c6d2ecc2c
AnthonyA <alicea.9a@gmail.com>
2024-11-19 15:23:38 -0700

    Refactor: Changed setupInput function to use connection return value - user inputs from stdin can now interact with the server directly - handleUserInput now filters for 'w,a,s,d' keys and sends movement commands to the server to control snake

--- input.js
+++ input.js
@@ -1,6 +1,7 @@
 const net = require("net");
-
-const setupInput = function () {
+let connection;
+const setupInput = function (conn) {
+  connection = conn;
   const stdin = process.stdin;  // create variable to hold the stdin object so we don't have to type process.stdin multiple times
   stdin.setRawMode(true); // Raw Mode allows us to listen for individual keypresses instead of waiting for the user to press enter
   stdin.setEncoding("utf8"); // utf8 encoding is set so that we can read the text data that is input
@@ -10,6 +11,10 @@ const setupInput = function () {
 };
 
 const handleUserInput = function(key){
+  const movement = {w: "Move: up", a: "Move: left", s: "Move: down", d: "Move: right"};
+  if (movement[key]){
+    connection.write(movement[key]);
+  }
   if(key === "\u0003"){
     process.exit();
   }
