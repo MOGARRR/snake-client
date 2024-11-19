728df3eaea43f0fc0c035ac6c0e027485feafd45
AnthonyA <alicea.9a@gmail.com>
2024-11-19 13:43:04 -0700

    feat: Added server connection confirmation and client name to client.js - Confirmation to provides user assurance of connection or point of possible error - Name to identify users their snake on screen and for future multiplayer use. Plus snakes like to have names too'

--- client.js
+++ client.js
@@ -4,6 +4,12 @@ const connect = function () {
     host: 'localhost',
     port: 50541,
   });
+
+  conn.on('connect', () => { // on connection it will log success and users name
+    console.log('Successfully connected to game server!');
+    conn.write('Name: ABA');
+  });
+
   // interpret incoming data as text
   conn.setEncoding("utf8"); // translates raw data to human text
   conn.on('data', (data) => { // listens for data from server and logs it to client console
