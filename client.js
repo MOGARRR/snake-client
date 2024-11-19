6fdcefc1ae2b569c6f7648e6aae9c75657407926
AnthonyA <alicea.9a@gmail.com>
2024-11-19 13:12:17 -0700

    #refactor: moved play.js function to new client.js file - Clears up play.js file for more focused tasks - maitaining client connection should prove easier to maintain and implement

---
+++ client.js
@@ -0,0 +1,15 @@
+const net = require("net"); // grabs npm net package
+const connect = function () {
+  const conn = net.createConnection({ // creates client connection object
+    host: 'localhost',
+    port: 50541,
+  });
+  // interpret incoming data as text
+  conn.setEncoding("utf8"); // translates raw data to human text
+  conn.on('data', (data) => { // listens for data from server and logs it to client console
+    console.log("Server says:",data);
+  });
+
+  return conn;
+};
+module.exports = connect;
