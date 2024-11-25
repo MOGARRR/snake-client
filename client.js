b783286151a0d0fde6119289eaba99c6dd720b63
AnthonyA <alicea.9a@gmail.com>
2024-11-25 10:39:38 -0700

    feat: Added second connection object and server for user to connect to - keeps count of new connections to server - sends message to all clients in server of new connection (except the joining connection)

--- client.js
+++ client.js
@@ -1,22 +1,33 @@
-const {IP,PORT} = require('./constants');
+const {IP,PORT1,PORT2} = require('./constants');
 const net = require("net"); // grabs npm net package
-const connect = function () {
-  const conn = net.createConnection({ // creates client connection object
+const args = process.argv;
+args.splice(0,2);
+
+const connect = function() {
+  const conn1 = net.createConnection({ // creates client connection object to connext
     host: IP,
-    port: PORT,
+    port: PORT1
+  });
+  const conn2 = net.createConnection({ // creates client for open server to get multiplayer info
+    host: IP,
+    port: PORT2,
   });
 
-  conn.on('connect', () => { // on connection it will log success and users name
+  conn1.on('connect', () => { // on connection it will log success and users name
     console.log('Successfully connected to game server!');
-    conn.write('Name: ABA');
+    conn1.write(`Name: ABA`);
+  });
+  conn2.setEncoding('utf8');
+  conn2.on('data', (data) => {
+    console.log('Server:', data);
   });
 
   // interpret incoming data as text
-  conn.setEncoding("utf8"); // translates raw data to human text
-  conn.on('data', (data) => { // listens for data from server and logs it to client console
-    console.log("Server says:",data);
+  conn1.setEncoding("utf8"); // translates raw data to human text
+  conn1.on('data', (data) => { // listens for data from server and logs it to client console
+    console.log("Server:",data);
   });
 
-  return conn;
+  return conn1;
 };
 module.exports = connect;
